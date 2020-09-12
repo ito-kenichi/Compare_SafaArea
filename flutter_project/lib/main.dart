d9027455ab9d03043bb8ab47154d8dbef647df69
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-09-12 11:28:39 +0900

    add safe area

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -39,20 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: SafeArea(child: Container(color: Colors.red)),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
--- react_native_project/App.js
+++ react_native_project/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
+      <SafeAreaView style={styles.container} />
       <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image}
+              source={{uri: 'https://s23527.pcdn.co/wp-content/uploads/2017/03/wesaturate.jpg.optimal.jpg'}}
+            />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 1200,
+    height: 1000,
+    marginLeft: 10,
+    marginTop: 10,
+  }
 });
